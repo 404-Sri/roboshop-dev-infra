36987e3caa7a059c2ddb93455208a4781a6f270f
bindu sri <bindusriamballa@gmail.com>
2025-11-12 10:49:52 +0530

    message

--- 80-frontend-alb/variables.tf
+++ 80-frontend-alb/variables.tf
@@ -6,10 +6,10 @@ variable "environment" {
     default = "dev"
 }
 
-variable "domain_name" {
-    default = "daws-sri.fun"
-}
-
 variable "zone_id" {
     default = "Z051555125XYI0DQWESV5"
+}
+
+variable "domain_name" {
+    default = "daws-sri.fun"
 }
