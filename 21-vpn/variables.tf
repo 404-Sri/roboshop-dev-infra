9756f8b992e0476ebaf6a75e272aff2d2bf12401
bindu sri <bindusriamballa@gmail.com>
2025-11-15 22:00:52 +0530

    message

---
+++ 21-vpn/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws-sri.fun"
+}
+
+variable "zone_id" {
+    default = "Z051555125XYI0DQWESV5"
+}
